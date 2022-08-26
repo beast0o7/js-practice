7881aca6aa08847fc61e02be17ad613e70096ac8
MAHAD ANSARI <mahad@nimapinfotech.com>
2022-08-26 17:44:46 +0530

    .

--- test.js
+++ test.js
@@ -770,3 +770,19 @@
 // console.log(newCircle.getCircumference());
 // console.log(newCircle.fullDescription());
 // console.log(newCircle.count);
+
+
+// var myObject = {
+//     foo: "bar",
+//     func: function() {
+//         var self = this;
+//         console.log("outer func:  this.foo = " + this.foo);
+//         console.log("outer func:  self.foo = " + self.foo);
+//         (function() {
+//             console.log("inner func:  this.foo = " + this.foo);
+//             console.log("inner func:  self.foo = " + self.foo);
+//         })();
+//     }
+// };
+// myObject.func();
+
